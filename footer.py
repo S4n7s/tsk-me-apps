d79ff3c42fc43baf947a138d85d7f8617a04f93f
S4n7s <92510520+S4n7s@users.noreply.github.com>
2023-05-31 15:12:54 +0300

    Update footer.py

--- footer.py
+++ footer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/evn python
 from tkinter import Label, BOTTOM
+import version
 
 
 def add_footer():
-    footer = Label(text="©️2023, ТСК Мосэнерго", compound='bottom', fg="black", bg="#fff")
+    footer_text = "v." + version.current_version + " ©️ 2023, ТСК Мосэнерго"
+    footer = Label(text=footer_text, compound='bottom', fg="black", bg="#fff")
     footer.pack(side=BOTTOM, anchor='s', pady=10)
